24c28fb52970fb637b1b1ad4838d62aa029b2ca5
Ozgun Zor <ozgunemrezor@gmail.com>
2019-04-19 18:38:09 +0100

    Create Marvel API Models.

---
+++ LibrariesPrototype/Common/Models/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  MarvelAPIAppZor
+//
+//  Created by Ozgun Zor on 4/4/19.
+//  Copyright © 2019 Ozgun Zor. All rights reserved.
+//
+
+import Foundation
+
+public struct Story: Decodable {
+    public let id: Int
+    public let title: String?
+    public let description: String?
+}
+
