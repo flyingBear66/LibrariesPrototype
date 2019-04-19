6c036cd5801e5f0a7f92a8513333e841d96a7430
Ozgun Zor <ozgunemrezor@gmail.com>
2019-04-19 18:05:38 +0100

    Add base classes.

---
+++ LibrariesPrototype/Presentation/Controls/LTButton.swift
@@ -0,0 +1,13 @@
+//
+//  LTButton.swift
+//  LibrariesPrototype
+//
+//  Created by Ozgun Zor on 4/19/19.
+//  Copyright © 2019 Ozgun Zor. All rights reserved.
+//
+
+import UIKit
+
+class LTButton: UIButton {
+
+}
---
+++ LibrariesPrototype/Presentation/ViewModels/LTViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LTViewModel.swift
+//  LibrariesPrototype
+//
+//  Created by Ozgun Zor on 4/19/19.
+//  Copyright © 2019 Ozgun Zor. All rights reserved.
+//
+
+import UIKit
+
+class LTViewModel: NSObject {
+
+}
