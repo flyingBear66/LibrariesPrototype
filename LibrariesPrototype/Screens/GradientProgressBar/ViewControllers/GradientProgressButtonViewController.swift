0b958dda25bbea96c557ed6cf008a0c520fe8952
özgün emre zor <ozgunemrezor@gmail.com>
2019-04-24 23:48:03 +0100

    Refactor GradientLoadingBar Button as one button

--- LibrariesPrototype/Screens/GradientProgressBar/ViewControllers/GradientProgressButtonViewController.swift
+++ LibrariesPrototype/Screens/GradientProgressBar/ViewControllers/GradientProgressButtonViewController.swift
@@ -18,6 +18,28 @@ class GradientProgressButtonViewController: LTViewController {
     private let safeAreaGradientLoadingBarSafe = GradientLoadingBar(height: 10.0,
                                                                     isRelativeToSafeArea: true)
 
+    
+    let bottomGradientButtonView: LTGradientBarButton = {
+        let button = LTGradientBarButton()
+        button.title = "Bottom"
+        button.type = .bottom
+        return button
+    }()
+    
+    let centerGradientButton: LTGradientBarButton = {
+        let button = LTGradientBarButton()
+        button.title = "Center"
+        button.type = .top
+        return button
+    }()
+    
+    let topGradientButton: LTGradientBarButton = {
+        let button = LTGradientBarButton()
+        button.title = "Top"
+        button.type = .top
+        return button
+    }()
+    
     // MARK: - Variables
     private var viewModel: GradientProgressButtonViewModel!
 
@@ -35,8 +57,6 @@ class GradientProgressButtonViewController: LTViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
-        safeAreaGradientLoadingBarSafe.show()
-        safeAreaGradientLoadingBar.show()
     }
 
     // MARK: - Helpers
@@ -46,6 +66,41 @@ class GradientProgressButtonViewController: LTViewController {
     }
 
     func updateUI() {
+        view.addSubview(bottomGradientButtonView)
+        view.addSubview(topGradientButton)
+        view.addSubview(centerGradientButton)
+        
+        //
+        centerGradientButton.addConstraints([equal(self.view, \.heightAnchor, \.heightAnchor, ratio: 0.1),
+                                             equal(self.view, \.widthAnchor, \.widthAnchor, ratio: 0.3),
+                                             equal(self.view, \.centerXAnchor),
+                                             equal(self.view, \.centerYAnchor)])
+        
+        //
+        topGradientButton.addConstraints([equal(self.view, \.heightAnchor, \.heightAnchor, ratio: 0.1),
+                                             equal(self.view, \.widthAnchor, \.widthAnchor, ratio: 0.3),
+                                             equal(centerGradientButton, \.bottomAnchor, \.topAnchor, constant: -8),
+                                             equal(self.view, \.centerXAnchor)])
+        
+        //
+        bottomGradientButtonView.addConstraints([equal(self.view, \.heightAnchor, \.heightAnchor, ratio: 0.1),
+                                                 equal(self.view, \.widthAnchor, \.widthAnchor, ratio: 0.3),
+                                                 equal(centerGradientButton, \.topAnchor, \.bottomAnchor, constant: 8),
+                                                 equal(self.view, \.centerXAnchor)])
+        
     }
+    
     // MARK: - UI Actions
+    
+    @objc func topGradientButtonTapped() {
+//        topGradientButton.toggle()
+    }
+    
+    @objc func centerGradientButtonTapped() {
+//        centerGradientButton.toggle()
+    }
+    
+    @objc func bottomGradientButtonTapped() {
+//        centerGradientButton.toggle()
+    }
 }
